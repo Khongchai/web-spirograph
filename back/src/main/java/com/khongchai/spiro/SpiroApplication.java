fe1f83cab2eb15da5d351725cd0dc532092aada6
k.greesuradej <world1955@hotmail.com>
2022-05-29 23:27:28 +0700

    added a starter backend folder

---
+++ back/src/main/java/com/khongchai/spiro/SpiroApplication.java
@@ -0,0 +1,13 @@
+package com.khongchai.spiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpiroApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/khongchai/spiro/SpiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khongchai.spiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
