49d7ac40724660c42c4fc680f49b70fc8a9dc079
k.greesuradej <world1955@hotmail.com>
2022-06-28 22:25:58 +0700

    i hate forms

--- front/src/components/main/Auth/LoginRegisterForm.tsx
+++ front/src/components/main/Auth/LoginRegisterForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 
 export function LoginRegisterForm({
   onFormSubmit,
@@ -7,15 +7,21 @@ export function LoginRegisterForm({
 }) {
   const [inputEmail, setInputEmail] = useState<string>();
   const [inputUsername, setInputUsername] = useState<string>();
+
+  const _onFormSubmit = (e: FormEvent) => {
+    onFormSubmit();
+    e.preventDefault();
+  };
+
   return (
-    <form className="w-full max-w-sm">
+    <form className="w-full max-w-sm" onSubmit={_onFormSubmit}>
       <div className="md:flex md:items-center mb-6">
         <div className="md:w-1/3">
           <label
             className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
             htmlFor="inline-full-name"
           >
-            Full Name
+            Email
           </label>
         </div>
         <div className="md:w-2/3">
@@ -33,7 +39,7 @@ export function LoginRegisterForm({
             className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
             htmlFor="inline-password"
           >
-            Password
+            Username
           </label>
         </div>
         <div className="md:w-2/3">
@@ -57,7 +63,7 @@ export function LoginRegisterForm({
         <div className="md:w-2/3">
           <button
             className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-            type="button"
+            type="submit"
           >
             Sign Up
           </button>
