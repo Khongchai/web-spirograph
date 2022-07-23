e1b22afe5e750ea76fbd0fa0069c98331856cc30
k.greesuradej <world1955@hotmail.com>
2022-07-23 21:53:27 +0700

    why is it so hard to mock a god damn database

--- back2/src/user/user.service.spec.ts
+++ back2/src/user/user.service.spec.ts
@@ -1,23 +1,39 @@
-import { Test } from '@nestjs/testing';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
+import { SavedConfiguration } from 'src/models/SavedConfiguration';
+import { MongoRepository } from 'typeorm';
 import { User } from '../models/User';
 import { UserService } from './user.service';
-jest.mock('./user.service');
-
-interface MockUser extends User {}
-class MockUser {}
 
 describe('UserService', () => {
   let userService: UserService;
-  const mockUser = new MockUser();
+  let mockRepository: MongoRepository<User>;
+  const mockedUser = new User({
+    email: 'mock@email.com',
+    savedConfigurations: [
+      new SavedConfiguration({
+        data: '',
+      }),
+    ],
+    username: '',
+  });
 
   beforeEach(async () => {
-    const moduleRef = await Test.createTestingModule({
-      imports: [TypeOrmModule.forFeature([User])],
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'postgres',
+          database: ':memory:',
+          dropSchema: true,
+          synchronize: true,
+          logging: false,
+        }),
+        TypeOrmModule.forFeature([User]),
+      ],
       providers: [UserService],
-      exports: [UserService],
     }).compile();
 
+    mockRepository = await moduleRef.resolve(getRepositoryToken(User));
     userService = await moduleRef.resolve(UserService);
   });
 
@@ -25,11 +41,10 @@ describe('UserService', () => {
     const mockEmail = 'mock@email.com';
     jest
       .fn(() => userService.findOne({ email: mockEmail }))
-      .mockReturnValueOnce(Promise.resolve(mockUser));
-
+      .mockReturnValueOnce(Promise.resolve(mockedUser));
     it('Should returned the queried user', async () => {
       const user = await userService.findOne({ email: mockEmail });
-      expect(user).toBe(mockUser);
+      expect(user).toBe(mockedUser);
     });
   });
 });
