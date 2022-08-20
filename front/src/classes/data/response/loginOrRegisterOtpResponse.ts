906d1caff746462125071a97b6ed9be01c52ffb6
k.greesuradej <world1955@hotmail.com>
2022-08-20 20:36:35 +0700

    Removed savedConfigurations from LogInOrRegisterResponse

--- front/src/classes/data/response/loginOrRegisterOtpResponse.ts
+++ front/src/classes/data/response/loginOrRegisterOtpResponse.ts
@@ -1,8 +1,5 @@
-import { User } from "../../domain/userData/User";
-
 export interface LogInOrRegisterOtpResponse {
   email: string;
   accessToken: string;
   username: string;
-  savedConfigurations: string[];
 }
