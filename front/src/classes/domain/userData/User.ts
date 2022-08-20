6728de4b62721e3730fd1735fec40384bcdf33b3
k.greesuradej <world1955@hotmail.com>
2022-08-20 20:35:38 +0700

    Removed configs from user. Configs is now a global state.

--- front/src/classes/domain/userData/User.ts
+++ front/src/classes/domain/userData/User.ts
@@ -3,7 +3,6 @@ import CycloidControls from "../cycloidControls";
 export class User {
   readonly username: string;
   readonly email: string;
-  readonly currentConfigs: CycloidControls[];
 
   constructor(user: User) {
     Object.assign(this, user);
