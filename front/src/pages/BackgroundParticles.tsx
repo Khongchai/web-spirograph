e0d3c5efcf64b62bd834161033f161b4d6edf1be
k.greesuradej <world1955@hotmail.com>
2022-08-23 22:50:27 +0700

    now using set canvas size manager for particles canvas

--- front/src/pages/BackgroundParticles.tsx
+++ front/src/pages/BackgroundParticles.tsx
@@ -1,29 +1,42 @@
-import React, { useRef } from "react";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { NaivgationStage } from "../types/Stage";
 import useOnMouseMove from "../utils/BackgroundParticles/useOnMouseMove";
-import useOnResize from "../utils/BackgroundParticles/useOnResize";
 import useOnStageChanged from "../utils/BackgroundParticles/useOnStageChanged";
 import useSetupParticlesWorker from "../utils/BackgroundParticles/useSetupParticlesWorker";
 import { CanvasSizeManagers } from "../utils/CanvasSizeManager";
+import ParticlesWorkerPayload, {
+  ParticlesWorkerOperation,
+} from "../Workers/BackgroundParticles/particlesWorkerPayloads";
 
-// Assume canvas is always the same size as the window.
-
-//TODO refactor into separate hooks and cancel the eventlisteners.
 export default function BackgroundParticles({
   stage,
-
 }: {
   stage: NaivgationStage;
 }) {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
+  function onResize() {
+    const resizePayload: ParticlesWorkerPayload = {
+      operation: ParticlesWorkerOperation.Resize,
+      resizePayload: {
+        newHeight: window.innerHeight,
+        newWidth: window.innerWidth,
+      },
+    };
+    worker!.postMessage(resizePayload);
+  }
+
   const { worker } = useSetupParticlesWorker({ canvasRef, dependencyList: [] });
 
-  useOnResize({
-    worker,
-    dependencyList: [worker],
-  });
+  useEffect(() => {
+    if (canvasRef.current && worker) {
+      CanvasSizeManagers.particlesWorkerCanvasSizeManager.setCanvasSize(
+        canvasRef.current,
+        onResize,
+        true
+      );
+    }
+  }, [worker]);
 
   useOnMouseMove({
     worker,
