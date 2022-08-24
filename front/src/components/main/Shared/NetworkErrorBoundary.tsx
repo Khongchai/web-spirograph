3dad82fc6c0cafd49659ef712abcf2391c8c79c1
k.greesuradej <world1955@hotmail.com>
2022-08-24 22:38:18 +0700

    Addd override

--- front/src/components/main/Shared/NetworkErrorBoundary.tsx
+++ front/src/components/main/Shared/NetworkErrorBoundary.tsx
@@ -3,8 +3,8 @@ import { NetworkError } from "../../../classes/customEvents";
 
 //TODO not yet working.
 export class NetworkErrorBoundary extends Component {
-  state: { hasError: boolean; details?: Response };
-  props: { children: ReactNode };
+  override state: { hasError: boolean; details?: Response };
+  override props: { children: ReactNode };
 
   constructor(props: { children: ReactNode }) {
     super(props);
@@ -13,7 +13,7 @@ export class NetworkErrorBoundary extends Component {
     this.state = { hasError: false };
   }
 
-  componentDidCatch(error: Error, _: ErrorInfo) {
+  override componentDidCatch(error: Error, _: ErrorInfo) {
     console.log("catch");
     this.setState({
       hasError: true,
@@ -21,6 +21,6 @@ export class NetworkErrorBoundary extends Component {
     });
   }
 
-  render = () =>
+  override render = () =>
     this.state.hasError ? <h1> Something went wrong.</h1> : this.props.children;
 }
