5f28c8b7fbca7e528fe3a65a0f020cb46d821e83
khongchai <world1955@hotmail.com>
2022-10-31 18:25:39 +0700

    stored date

--- back2/src/models/SavedConfiguration.ts
+++ back2/src/models/SavedConfiguration.ts
@@ -4,11 +4,15 @@ export class SavedConfiguration {
   //Just a unique identifier, as this is not a typeORM entity
   id: string;
 
+  // Milliseconds
+  date: number;
+
   data: string;
 
   constructor({ id, data }: { id?: string; data?: string }) {
     Object.assign(this, {
       id: id ?? randomUUID(),
+      date: Date.now(),
       data: data,
     } as SavedConfiguration);
   }
