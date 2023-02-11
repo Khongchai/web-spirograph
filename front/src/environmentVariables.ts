590cdbd137b07156bc3cf9f0d5c93784bac9e6d3
khongchai <world1955@hotmail.com>
2023-02-11 23:56:41 +0700

    did stuff

--- front/src/environmentVariables.ts
+++ front/src/environmentVariables.ts
@@ -1,6 +1,6 @@
 export const { REACT_APP_BASE_API_ENDPOINT } = process.env;
 
-if (!REACT_APP_BASE_API_ENDPOINT) {
-  console.info(process.env);
-  throw new Error("BASE_API_ENDPOINT is not set in .env");
-}
+// if (!REACT_APP_BASE_API_ENDPOINT) {
+  // console.info(process.env);
+  // throw new Error("BASE_API_ENDPOINT is not set in .env");
+// }
